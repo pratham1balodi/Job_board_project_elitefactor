09a2434abf290c406f7bd9c23d9e6f5875e6f502
pratham1balodi <95518381+pratham1balodi@users.noreply.github.com>
2026-02-04 23:40:22 +0530

    Update views.py

--- jobs/views.py
+++ jobs/views.py
@@ -4,9 +4,9 @@ from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from .models import JobPost, Application
-from .forms import JobPostForm
 from users.models import User
 
+# --- 1. Public Homepage ---
 class HomepageView(TemplateView):
     template_name = 'homepage.html'
     def get_context_data(self, **kwargs):
@@ -19,6 +19,7 @@ class HomepageView(TemplateView):
             context['total_employers'] = 0
         return context
 
+# --- 2. Job Listing & Detail ---
 class JobListView(ListView):
     model = JobPost
     template_name = 'jobs/job_list.html'
@@ -29,26 +30,34 @@ class JobListView(ListView):
 class JobDetailView(DetailView):
     model = JobPost
     template_name = 'jobs/job_detail.html'
-    context_object_name = 'job'
 
+# --- 3. SEEKER DASHBOARD (This fixes your AttributeError) ---
+class SeekerDashboardView(LoginRequiredMixin, ListView):
+    model = Application
+    template_name = 'jobs/seeker_dashboard.html'
+    context_object_name = 'applications'
+
+    def get_queryset(self):
+        # Shows jobs this specific user applied for
+        return Application.objects.filter(applicant=self.request.user).order_by('-applied_at')
+
+# --- 4. EMPLOYER DASHBOARD ---
 class EmployerDashboardView(LoginRequiredMixin, ListView):
     model = JobPost
     template_name = 'jobs/employer_dashboard.html'
     context_object_name = 'jobs'
-    def get_queryset(self):
-        return JobPost.objects.filter(employer=self.request.user)
 
+    def get_queryset(self):
+        return JobPost.objects.filter(employer=self.request.user).order_by('-created_at')
+
+# --- 5. Job Creation (Employer only) ---
+from .forms import JobPostForm
 class JobCreateView(LoginRequiredMixin, CreateView):
     model = JobPost
     form_class = JobPostForm
     template_name = 'jobs/job_form.html'
     success_url = reverse_lazy('employer_dashboard')
+
     def form_valid(self, form):
         form.instance.employer = self.request.user
         return super().form_valid(form)
-
-def toggle_job_status(request, pk):
-    job = get_object_or_404(JobPost, pk=pk, employer=request.user)
-    job.is_active = not job.is_active
-    job.save()
-    return redirect('employer_dashboard')
