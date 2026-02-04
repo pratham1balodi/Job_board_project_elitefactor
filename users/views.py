81830fc0c4b711b5f270b9965d771899dbe6f4ef
pratham1balodi <95518381+pratham1balodi@users.noreply.github.com>
2026-02-04 22:13:20 +0530

    Update views.py

--- users/views.py
+++ users/views.py
@@ -1,36 +1,19 @@
-# File: job_board_project_final/users/views.py
+# File: job_board_project_final/users/forms.py
 
-from django.shortcuts import render, redirect
-from django.contrib.auth import login
-from django.views.generic import CreateView, TemplateView
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.urls import reverse_lazy
-from django.contrib import messages
-
-# Local imports from your users app
-from .forms import UserRegistrationForm
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
-class SignUpView(CreateView):
-    """Handles new user registration with role selection."""
-    model = User
-    form_class = UserRegistrationForm
-    # Path updated to point to your 'users' subfolder
-    template_name = 'users/signup.html' 
-    success_url = reverse_lazy('login')
+class UserRegistrationForm(UserCreationForm):
+    """Form for creating a new user with a specific role."""
+    
+    class Meta(UserCreationForm.Meta):
+        model = User
+        # Ensure 'role' is included so users can choose Seeker or Employer
+        fields = UserCreationForm.Meta.fields + ('role', 'first_name', 'last_name', 'email')
 
-    def form_valid(self, form):
-        user = form.save()
-        messages.success(self.request, f"Account created for {user.username}! You can now log in.")
-        return super().form_valid(form)
-
-class ProfileView(LoginRequiredMixin, TemplateView):
-    """Displays user profile and redirects to appropriate dashboard."""
-    # Path updated to point to your 'users' subfolder
-    template_name = 'users/profile.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        # Pass the user object to the template
-        context['user'] = self.request.user
-        return context
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Adding Bootstrap classes for better UI
+        for field in self.fields.values():
+            field.widget.attrs.update({'class': 'form-control'})
