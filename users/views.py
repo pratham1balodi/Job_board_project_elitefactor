7cf7e95f7b2d9db243f6b9057734a73c6ad67363
pratham1balodi <95518381+pratham1balodi@users.noreply.github.com>
2026-02-04 23:21:27 +0530

    Update views.py

--- users/views.py
+++ users/views.py
@@ -1,8 +1,6 @@
-# File: job_board_project_final/users/views.py
 from django.views.generic import CreateView, TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.contrib import messages
 from .forms import UserRegistrationForm
 from .models import User
 
@@ -11,11 +9,6 @@ class JobSeekerSignUpView(CreateView):
     form_class = UserRegistrationForm
     template_name = 'users/seeker_signup.html'
     success_url = reverse_lazy('login')
-
-    def form_valid(self, form):
-        response = super().form_valid(form)
-        messages.success(self.request, "Account created! You can now log in.")
-        return response
 
 class ProfileView(LoginRequiredMixin, TemplateView):
     template_name = 'users/profile.html'
