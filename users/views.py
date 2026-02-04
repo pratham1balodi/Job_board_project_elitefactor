de6c5f14701843617cfefb42907a749e8e399269
pratham1balodi <95518381+pratham1balodi@users.noreply.github.com>
2026-02-04 22:40:42 +0530

    Update views.py

--- users/views.py
+++ users/views.py
@@ -1,15 +1,32 @@
-# File: job_board_project_final/users/urls.py
+# File: job_board_project_final/users/views.py
+from django.shortcuts import render, redirect
+from django.views.generic import CreateView, TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.contrib import messages
+from .forms import UserRegistrationForm
+from .models import User
 
-from django.urls import path
-from django.contrib.auth import views as auth_views
-from .views import SignUpView, ProfileView  # Use the names from your views.py
+class SignUpView(CreateView):
+    """Main Registration View."""
+    model = User
+    form_class = UserRegistrationForm
+    template_name = 'users/signup.html'
+    success_url = reverse_lazy('login')
 
-urlpatterns = [
-    # Registration & Profile
-    path('signup/', SignUpView.as_view(), name='signup'),
-    path('profile/', ProfileView.as_view(), name='profile'),
+    def form_valid(self, form):
+        user = form.save()
+        messages.success(self.request, f"Account created for {user.username}!")
+        return super().form_valid(form)
 
-    # Authentication (Standard Django Views)
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='homepage'), name='logout'),
-]
+# Alias to fix the ImportError you saw
+JobSeekerSignUpView = SignUpView 
+
+class ProfileView(LoginRequiredMixin, TemplateView):
+    """User Profile View."""
+    template_name = 'users/profile.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user'] = self.request.user
+        return context
