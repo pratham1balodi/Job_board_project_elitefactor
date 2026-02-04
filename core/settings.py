b4cc518fa0524ac050fd7058a34ae128e3521483
pratham1balodi <95518381+pratham1balodi@users.noreply.github.com>
2026-02-04 21:23:57 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-for-now')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
