29376035b64a1260b94cee4cc473ef5b637fa4ae
pratham1balodi <95518381+pratham1balodi@users.noreply.github.com>
2026-02-04 22:46:47 +0530

    Update views.py

--- users/views.py
+++ users/views.py
@@ -1,4 +1,3 @@
-# File: job_board_project_final/users/views.py
 from django.shortcuts import render, redirect
 from django.views.generic import CreateView, TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -8,26 +7,16 @@ from .forms import UserRegistrationForm
 from .models import User
 
 class JobSeekerSignUpView(CreateView):
-    """Registration View using your existing seeker_signup.html template."""
     model = User
     form_class = UserRegistrationForm
-    # FIX: Pointing specifically to the file you have on GitHub
+    # This points to the exact file you have on GitHub
     template_name = 'users/seeker_signup.html' 
     success_url = reverse_lazy('login')
 
     def form_valid(self, form):
         user = form.save()
-        messages.success(self.request, f"Account created for {user.username}!")
+        messages.success(self.request, "Registration successful! Please login.")
         return super().form_valid(form)
 
-# Alias to ensure other parts of the app don't crash
-SignUpView = JobSeekerSignUpView 
-
 class ProfileView(LoginRequiredMixin, TemplateView):
-    """User Profile View."""
     template_name = 'users/profile.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['user'] = self.request.user
-        return context
