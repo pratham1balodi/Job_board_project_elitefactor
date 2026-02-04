ef27cc3f7c5e29d179c4e6407d25fd2f494e5f16
pratham1balodi <95518381+pratham1balodi@users.noreply.github.com>
2026-02-04 22:51:11 +0530

    Update views.py

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+# File: job_board_project_final/users/views.py
 from django.views.generic import CreateView, TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
@@ -9,14 +9,13 @@ from .models import User
 class JobSeekerSignUpView(CreateView):
     model = User
     form_class = UserRegistrationForm
-    # This points to the exact file you have on GitHub
-    template_name = 'users/seeker_signup.html' 
+    template_name = 'users/seeker_signup.html'
     success_url = reverse_lazy('login')
 
     def form_valid(self, form):
-        user = form.save()
-        messages.success(self.request, "Registration successful! Please login.")
-        return super().form_valid(form)
+        response = super().form_valid(form)
+        messages.success(self.request, "Account created! You can now log in.")
+        return response
 
 class ProfileView(LoginRequiredMixin, TemplateView):
     template_name = 'users/profile.html'
